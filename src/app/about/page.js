22f1134b9ba2ac32fd796949d3409216cba975e2
adityawintech <aditya#19AI>
2025-06-20 18:25:26 +0530

    Aditya

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,6 @@
-import AboutHero from "../../components/about/AboutHero"
-import AboutContent from "@/components/about/AboutContent"
-import AboutCTA from "@/components/about/AboutCTA"
+import AboutHero from "../../components/about/AboutHero.jsx"
+import AboutContent from "@/components/about/AboutContent.jsx"
+import AboutCTA from "@/components/about/AboutCTA.jsx"
 
 const AboutPage=()=> {
   return (
